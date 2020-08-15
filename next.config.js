f55e047798e7695763d3cc1dad8e3dd81cfb69c1
yamachan0625 <napoli.080625@gmail.com>
2020-08-16 04:06:28 +0900

    next.confing.jsでproxyの設定

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 /* eslint-disable
     @typescript-eslint/no-var-requires,
     @typescript-eslint/explicit-function-return-type
-*/ const path = require('path');
+*/
+
+const path = require('path');
+
+const ROOT_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://awseb-awseb-t7h2otj93b0q-2085495435.ap-northeast-1.elb.amazonaws.com'
+    : 'http://localhost:4000';
 
 module.exports = {
   webpack: (config) => {
@@ -11,5 +18,14 @@ module.exports = {
       '~': path.resolve(__dirname, './src'),
     };
     return config;
+  },
+  async rewrites() {
+    return [
+      { source: '/graphql', destination: `${ROOT_URL}/graphql` },
+      {
+        source: '/playground',
+        destination: `${ROOT_URL}/playground`,
+      },
+    ];
   },
 };
