f2620a6bea37d9fbaa570813d384cd9cd9e2bb95
yamachan0625 <napoli.080625@gmail.com>
2020-10-26 23:40:44 +0900

    git rm -rで小文字先頭コンポーネントファイル削除4

--- src/components/templates/mainTemplateStyle.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-    },
-  })
-);
