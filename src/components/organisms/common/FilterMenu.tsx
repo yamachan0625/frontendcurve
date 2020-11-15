735cfcbde2c38e41127c04e3ff5d953c3daa5baf
yamachan0625 <napoli.080625@gmail.com>
2020-11-16 01:16:15 +0900

    filterMenuStyle分離

--- src/components/organisms/common/FilterMenu.tsx
+++ src/components/organisms/common/FilterMenu.tsx
@@ -6,32 +6,11 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    accordionRoot: {
-      backgroundColor: '#3f3f3f',
-      color: theme.palette.primary.contrastText,
-      boxShadow: 'none',
-      borderBottom: `2px solid ${theme.palette.primary.dark}`,
-      '&:before': {
-        height: '0px',
-      },
-    },
-    filterTitle: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    filterIcon: { color: theme.palette.primary.contrastText },
-    span: {
-      marginLeft: theme.spacing(1),
-    },
-  })
-);
+import { useStyles } from './FilterMenuStyle';
 
 export const FilterMenu = ({ children }) => {
   const classes = useStyles();
+
   return (
     <Accordion className={classes.accordionRoot}>
       <AccordionSummary
